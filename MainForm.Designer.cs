114041c52e87e389b6b599db66d3337a01ff8ba0
YYLMZXC <YYLMZXC@163.com>
2025-10-27 04:19:06 +0800

    1

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,14 +30,16 @@ namespace ScreenControl
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(235, 187);
+            this.button2.Location = new System.Drawing.Point(176, 150);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(268, 62);
+            this.button2.Size = new System.Drawing.Size(201, 50);
             this.button2.TabIndex = 1;
             this.button2.Text = "立刻关闭屏幕";
             this.button2.UseVisualStyleBackColor = true;
@@ -45,10 +47,12 @@ namespace ScreenControl
             // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 360);
             this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "MainForm";
             this.Text = "屏幕控制";
             this.Load += new System.EventHandler(this.MainForm_Load_1);
